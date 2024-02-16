ab216d30b72e91b7b78ff433f298cf5b8b4781e8
sangmihwang <102012985+sangmihwang@users.noreply.github.com>
2024-02-16 20:58:37 +0900

    Title: 겹치지 않게 선분 고르기, Time: 165ms, Memory: 26MB, Status: Passed - CodeTree

--- 240216/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240216/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -1,19 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-def sol(i, s, e):
-    global cnt, maxCnt
+def sol(i):
+    global cnt, maxCnt, newS, newE
     if i == n:
         if cnt > maxCnt:
             maxCnt = cnt
         return
     newS, newE = lines[i][0], lines[i][1]
-    if newS > e:
+    if overlapNone(selected, newS, newE):
         cnt += 1
-        sol(i+1, newS, newE)
+        selected.append([newS, newE])
+        sol(i+1)
+        selected.pop()
         cnt -= 1
-        sol(i+1, s, e)
+        sol(i+1)
+    else:
+        sol(i+1)
 
+def overlapNone(selected, i, j):
+    flag = True
+    for line in selected:
+        if not (i < j < line[0] < line [1] or line[0] < line[1] < i < j):
+            flag = False            # 하나라도 겹치는게 나오면 False
+    return flag
 
 n = int(input())
 lines = []
@@ -22,5 +32,6 @@ for _ in range(n):
 lines.sort()
 i = 0
 cnt, maxCnt = 0, 0
-sol(0, -1, 0)
+selected = []
+sol(0)
 print(maxCnt)
